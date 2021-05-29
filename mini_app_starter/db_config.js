5505cd1f57af444e7e1c0876e85a60be1770fc43
Niki <nicolette88@gmail.com>
2021-05-29 15:09:38 +0200

    órai feladat - mini app

---
+++ mini_app_starter/db_config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyASubz3P2EAy4HqGoVo2-v2KtpBlR5El_A',
+  authDomain: 'chat-app-wsuf-niki.firebaseapp.com',
+  databaseURL: 'https://chat-app-wsuf-niki.firebaseio.com',
+  projectId: 'chat-app-wsuf-niki',
+  storageBucket: 'chat-app-wsuf-niki.appspot.com',
+  messagingSenderId: '659542811895'
+};
+
+export default config;
